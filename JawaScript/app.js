9d61f066d757534205bb3af622d78ec4536e36a9
SyedMudassir1 <122387591+SyedMudassir1@users.noreply.github.com>
2023-03-20 13:09:44 -0700

    Add files via upload

---
+++ JawaScript/app.js
@@ -0,0 +1,112 @@
+// var str = "hello, my name is johar abbas";
+// for(var i = 0; i < str.length; i++){
+//  if (str.slice(i,i + 5) == 'johar ') {
+//   str = (str.slice(0, i)) + "ghous" + " " + str.slice(i)
+//   break;
+//   }
+// }
+// console.log(str);
+
+
+// var str = "hello, my name is johar abbas";
+// for(var i = 0; i < str.length; i++){
+//  if (str.slice(i,i + 5) == 'johar') {
+//   str = (str.slice(0, i)) + "smit" + " " + str.slice(i)
+//   break;
+//   }
+// }
+// console.log(str);
+
+// var num = 35.36;
+// console.log(word)
+
+
+// var food = prompt("WellCome to ABC Bakery.What do you want to order Sir/Ma'am");
+// food = food.toLowerCase()
+// var arr = ["cake" , "apple pie" , "cookie" , "chips" , "patties"]
+// for (var i = 0; i > arr.length; i++){
+//   if(arr[i] === food){
+//     alert("cookie is available")
+//   }else{alert("We are sorry")}
+//   break;
+// }
+
+
+// ====================  Chp # 31  =============================
+
+//------------ GETTING THE CURRENT DATE AND TIME -------------------
+
+// var a = newDate(0,3);
+// console.log(a); 
+
+// var a = new Date();
+// console.log(a);
+
+// var a = new Date ();
+// var b = a.toString();
+// var c = b.slice(0,3)
+// console.log(a.toString())
+
+// var dob = new Date(prompt("enter your date of birth", " jan 1, 2005"));
+// var dobmili = dob.getTime();
+// var today = new Date();
+// var todaymili = today.getTime()
+// var diff = todaymili - dobmili;
+// var accuage = math.floor(diff/(1000*60*60*24*30*12))
+// document.write(accuage);
+
+// var a = new Date();
+// console.log(a);
+  
+// finding hours 
+
+// var d = new Date();
+//  var mytime = d.getHours();
+//  document.write(mytime);
+
+// finding date 
+
+// var a = new Date();
+// var myDate = a.getDate();
+// document.write(myDate);
+
+// finding Time 
+// var t = new Date();
+// var myTime = t.getTime();
+// document.write(myTime);
+
+// finding day 
+
+// var d = new Date();
+//  var myday = d.getDay();
+//  document.write(myday);
+
+// var d = new Date();
+//  var myday = d.getDay();
+//  if(myday == 0)
+//  {
+//  document.write('Sunday');
+//  } 
+ 
+// else if (myday == 1)
+//  {
+//  document.write('Monday');
+//  }
+//  else if (myday == 2)
+//  {
+//  document.write('Tuesday');
+//  }
+//  else if (myday == 3)
+//  {
+//  document.write('Wednesday');
+//  }
+//  else if (myday == 4)
+//  {
+//  document.write('Thursday');
+//  }
+//  else if (myday == 5)
+//  {
+//  document.write('Friday');
+//  }
+//  else
+//  { document.write('Saturday'); }
